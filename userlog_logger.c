bd5502c4fade1d3055c88b3666f0ef97bbffb509
thahn999 <thahn999@jam2in.com>
2016-03-23 11:58:34 +0900

    add pthread log_lock

--- userlog_logger.c
+++ userlog_logger.c
@@ -1,4 +1,3 @@
-
 /* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 /*
  * arcus-memcached - Arcus memory cache server
@@ -27,6 +26,7 @@
 #include <time.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <pthread.h>
 #include "protocol_extension.h"
 
 static EXTENSION_LOG_LEVEL current_log_level = EXTENSION_LOG_WARNING;
@@ -36,10 +36,10 @@ static const char *get_name(void) {
     return "userlog";
 }
 
-/* Instead of syslog which needs su authority, make and use user's log files : 2016.02 */
-   /* make & use 5 log files, rotate the files, append the created date to the file name */
-   /* make a directory containing log files, named "/ARCUSlog" under the current directory */
-   /* definitions and data */
+/* Instead of syslog which needs su authority, make and use user's log files : 2016.02
+ * make & use 5 log files, rotate the files, append the created date to the file name
+ * make a directory containing log files, named "/ARCUSlog" under the current directory */
+/* definitions and data */
 #define NUM_LOGFILE 5
 #define MAX_LOGFILE_SIZE (1024*1024*10)  // for now, 10M
 #define DEFAULT_LOGFILE_NAME "arcus"
@@ -52,14 +52,16 @@ static int  current_flength;          // current file length
 static char prev_log[2048];           // reserve the previously printed log
 static char prev_time[200];           // previous log time
 static int  samelog_cnt;              // number of the same log
+pthread_mutex_t log_lock;             // userlog thread lock
 
 static void do_make_logname(int filenum, char *ret_name)
 {
-        /* log file name : DIR-NAME + "/" + DEFAULT-NAME + sequence no(0~4) + date + ".log" */
+    /* log file name : DIR-NAME + "/" + DEFAULT-NAME + sequence no(0~4) + date + ".log" */
     if (ret_name == NULL || filenum < 0 || filenum > NUM_LOGFILE)  return;
     char buf[20];
     sprintf(ret_name, "%s/%s", LOGDIRECTORY, DEFAULT_LOGFILE_NAME);
-    sprintf(buf, "%d_", filenum);      strcat(ret_name, buf);
+    sprintf(buf, "%d_", filenum);
+    strcat(ret_name, buf);
     struct tm *day;
     time_t clock = time(0);
     day = localtime(&clock);
@@ -69,7 +71,7 @@ static void do_make_logname(int filenum, char *ret_name)
 
 static void do_make_prefix(char *ret_string)
 {
-       /* prefix : syslog type : time(month(Eng) day hr:min:sec) + hostname + pid */
+    /* prefix : syslog type : time(month(Eng) day hr:min:sec) + hostname + pid */
     struct tm *now;
     time_t clock;
     char buf[50], hname[30];
@@ -95,10 +97,11 @@ static void logger_log(EXTENSION_LOG_LEVEL severity,
         int len = vsnprintf(body_buf, sizeof(body_buf), fmt, ap);
         va_end(ap);
 
-             /* userlog codes */
+        /* userlog codes */
+	pthread_mutex_lock(&log_lock);
         if ( (len!=strlen(prev_log)) || strcmp(body_buf, prev_log)!=0 ) {
-               /* Two log messages are different. Print the count and last time,
-                * then restart the count */
+            /* Two log messages are different. Print the count and last time,
+             * then restart the count */
             if ( samelog_cnt > 1 ) {
                 sprintf(full_buf, "%slast message repeated %d times\n",
                          prev_time, samelog_cnt);
@@ -134,9 +137,11 @@ static void logger_log(EXTENSION_LOG_LEVEL severity,
             current_flength = 0;
             if (current_fp == NULL) {
                 fprintf(stderr, "\n FATAL error : can't open user log file: %s\n", logfile_name[current_file]);
+	        pthread_mutex_unlock(&log_lock);
                 return ;
             }
         }
+	pthread_mutex_unlock(&log_lock);
     }
 }
 
@@ -162,7 +167,7 @@ EXTENSION_ERROR_CODE memcached_extensions_initialize(const char *config,
     }
     current_log_level = sapi->log->get_level();
 
-         /* userlog codes */
+    /* userlog codes */
     if ( mkdir(LOGDIRECTORY, 0744) == -1 ) {
         if (errno != EEXIST) {
             fprintf(stderr, "\n FATAL error : can't make log Directory: %s\n", LOGDIRECTORY);
@@ -176,9 +181,11 @@ EXTENSION_ERROR_CODE memcached_extensions_initialize(const char *config,
     }
     current_file = 0;
     current_flength = 0;
-    prev_log[0] = 0;   prev_time[0] = 0;
+    prev_log[0] = 0;
+    prev_time[0] = 0;
     samelog_cnt = 1;
-        /* end of userlog codes */
+    pthread_mutex_init(&log_lock, NULL);
+    /* end of userlog codes */
 
     if (!sapi->extension->register_extension(EXTENSION_LOGGER, &descriptor)) {
         return EXTENSION_FATAL;
